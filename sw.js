b00371309a87d839753f1f9ded94c6048088941a
mohammedhalim <elaskandrane77@gmail.com>
2025-12-15 12:02:03 +0200

    feat: Implement offline support with service worker
    Adds a service worker to enable offline functionality by caching essential assets. This includes HTML, manifest, CSS, and necessary JavaScript libraries. This change also updates the Tailwind CSS configuration to include a new slide-up animation and enforces portrait orientation for the app.

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'pocketfinance-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18.2.0/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js',
+  'https://unpkg.com/recharts@2.10.3/umd/Recharts.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn-icons-png.flaticon.com/512/1055/1055131.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
